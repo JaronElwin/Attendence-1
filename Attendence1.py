6587f76805e8262ca8217938bb25cc0fed5ed355
Jaron Devaraj <jaronelwin@gmail.com>
2025-10-18 13:11:41 +0530

    Update Attendence1.py

--- Attendence1.py
+++ Attendence1.py
@@ -6,3 +6,9 @@ class_attended = int(input("Enter the number of class attended by student:"))
 attendence = class_held / class_attended * 100
 print("Attendence in percentage is:", attendence)
 
+#Updated program to check eligibility
+
+if attendence >= 75:
+    print("Student is eligible for exams"):
+else:
+    print("You are not eligible for exams")
